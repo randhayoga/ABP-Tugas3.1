fcc659f86d98cf9281ee2b56d9158991d3ef8afc
randhayoga <92174528+randhayoga@users.noreply.github.com>
2024-03-13 16:50:58 +0700

    feat: calculate odd sequence

--- JavaScripts/script.js
+++ JavaScripts/script.js
@@ -1,17 +1,32 @@
 function calculateOddSequence(min, max) {
   let result = 0;
+  let i = min;
 
-  // Check if min is even, if it is then add 1 to it
-  if (min % 2 == 0) {
-    min++;
+  // Check if i (min) is even, if it is then add 1 to it
+  if (i % 2 == 0) {
+    i++;
   }
 
-  while (min <= max) {
-    result += min;
-    min += 2;
+  // Iteration for odd sequence summmation
+  while (i <= max) {
+    result += i;
+    i += 2;
   }
 
   return result;
 }
 
 // Main
+const calculateButton = document.getElementById("btn-calculate");
+
+calculateButton.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const min = parseInt(document.getElementById("min-value").value);
+  const max = parseInt(document.getElementById("max-value").value);
+  const resultDiv = document.getElementById("result-box");
+
+  const resultValue = calculateOddSequence(min, max);
+
+  resultDiv.innerHTML = `<input class="form-control" type="text" value="${resultValue}" aria-label="readonly input example" readonly></input>`;
+});
